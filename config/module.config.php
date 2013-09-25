a8ff6bb7ee579c4340df76e3af61867c1644595d
zf2timo <tewes@henworx.de>
2013-09-25 09:20:02 +0200

    Fixed error with duplicated isMaintenance() decleration

--- config/module.config.php
+++ config/module.config.php
@@ -2,30 +2,6 @@
 return array(
     'controllers' => array(
         'invokables' => array(
-            'tteMaintenance\Controller\Skeleton' => 'tteMaintenance\Controller\SkeletonController',
-        ),
-    ),
-    'router' => array(
-        'routes' => array(
-            'tteMaintenance' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    // Change this to something specific to your module
-                    'route'    => '/maintenance.html',
-                    'defaults' => array(
-                        // Change this value to reflect the namespace in which
-                        // the controllers for your module are found
-                        '__NAMESPACE__' => 'Maintenance\Controller',
-                        'controller'    => 'Maintenance',
-                        'action'        => 'index',
-                    ),
-                ),
-            ),
-        ),
-    ),
-    'view_manager' => array(
-        'template_path_stack' => array(
-            'Maintenance' => __DIR__ . '/../view',
         ),
     ),
 );
