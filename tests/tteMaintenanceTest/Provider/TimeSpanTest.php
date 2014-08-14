69219353b8ef0a1596cb584ec6c6f35610dad162
live <live@localhost.localdomain>
2014-08-14 22:06:08 +0200

    WIP: PHPUnit Test for TimeSpan Provider

---
+++ tests/tteMaintenanceTest/Provider/TimeSpanTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace tteMaintenanceTest\Provider;
+
+use tteMaintenance\Options\TimeSpanOptions;
+use tteMaintenance\Provider\TimeSpan;
+
+class TimeSpanTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers tteMaintenance\Provider\TimeSpan::isMaintenance
+     */
+    public function testIsMaintenanceWithCorrectValues()
+    {
+
+        $provider = new TimeSpan(22, 23);
+        $this->assertSame(true, $provider->isMaintenance());
+
+        $provider = new TimeSpan(22, 23);
+        $this->assertSame(false, $provider->isMaintenance());
+    }
+
+    /**
+     * @covers tteMaintenance\Provider\TimeSpan::isMaintenance
+     */
+    public function testIsMaintenanceWithString()
+    {
+
+        $provider = new TimeSpan('22', '23');
+        $this->assertSame(true, $provider->isMaintenance());
+
+        $provider = new TimeSpan('22', '23');
+        $this->assertSame(true, $provider->isMaintenance());
+    }
+}
+ 
